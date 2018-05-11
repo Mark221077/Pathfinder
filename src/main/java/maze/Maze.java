99ce99e40fb3c6f167bfdf96396384ceafb78025
Mark <pal.mark28@gmail.com>
2018-05-11 21:29:42 +0200

    finished MazeBuilder, print methods for Maze

--- src/main/java/maze/Maze.java
+++ src/main/java/maze/Maze.java
@@ -1,6 +1,6 @@
 package maze;
 
-import java.io.*;
+
 import java.util.ArrayList;
 
 public class Maze {
@@ -31,5 +31,36 @@ public class Maze {
 
     public void setTargetNode(Node targetNode) {
         this.targetNode = targetNode;
+    }
+
+    private ArrayList<ArrayList<Node> > nodeMatrix = new ArrayList<>();
+
+    public void setNodeMatrix(ArrayList<ArrayList<Node>> nodeMatrix) {
+        this.nodeMatrix = nodeMatrix;
+    }
+
+    public void drawMaze() {
+        if(nodeMatrix == null) {
+            System.out.println("Node matrix was null");
+        }
+    }
+
+    public String stringFormat() {
+        StringBuilder builder = new StringBuilder();
+        for(var list : nodeMatrix) {
+            for(var node : list) {
+                if(node == null)
+                    builder.append('#');
+                else if(node.isSourceNode())
+                    builder.append('S');
+                else if(node.isTargetNode())
+                    builder.append('X');
+                else
+                    builder.append('.');
+            }
+            builder.append('\n');
+        }
+
+        return builder.toString();
     }
 }
--- src/main/java/maze/MazeBuilder.java
+++ src/main/java/maze/MazeBuilder.java
@@ -1,72 +1,99 @@
 package maze;
 
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+
 
 public class MazeBuilder {
 
+
+    public Maze build(File file) {
+        var input = new ArrayList<String>();
+        try (BufferedReader r = new BufferedReader(new FileReader(file))){
+            String line;
+            while((line = r.readLine()) != null) {
+                if(line.length() > 0)
+                    input.add(line);
+            }
+        } catch (IOException ex) {
+            System.out.println("Couldn't read the file");
+        }
+
+        return build(input);
+    }
+
+
     public Maze build(ArrayList<String> input) {
         var maze = new Maze();
-        if(input == null || input.size() == 0)
+        if (input == null || input.size() == 0)
             return null;
 
 
         //check if the input has a rectangular shape, all rows are the same
         var width = input.get(0).length();
-        for(var row : input) {
-            if(width != row.length()) return maze;      //one row doesnt have the same length as the first one
+        for (var row : input) {
+            if (width != row.length()) return maze;      //one row doesnt have the same length as the first one
         }
 
         //convert the array list to 2D Node array/matrix,
-        var matrix = new ArrayList< ArrayList<Node> >(input.size());
+        var matrix = new ArrayList<ArrayList<Node>>(input.size());
 
         //initialize the row lists
-        for(int i = 0; i < input.size(); ++i)
+        for (int i = 0; i < input.size(); ++i)
             matrix.add(new ArrayList<>(width));
 
 
         int beginNodeCount = 0, targetNodeCount = 0;
 
+        Node sourceNode = null, targetNode = null;
+
+        //TODO: optimize this by converting to one iteration
+
         //fill the matrix
-        for(int row = 0; row < input.size(); ++row) {
-            var str = input.get(0);                     //a line with a row
-            for(int col = 0; col < str.length(); ++col) {   //iterate through the row
+        for (int row = 0; row < input.size(); ++row) {
+            var str = input.get(row);                     //a line with a row
+            for (int col = 0; col < str.length(); ++col) {   //iterate through the row
                 Node n = new Node(row, col);                //a new node, with the coordinates
                 char c = str.charAt(col);                   //the char in the current column
-                if(c == 'S'){
+                if (c == 'S') {
                     n.setSourceNode(true);         //source
                     ++beginNodeCount;               //test for multiple source nodes
-                }
-                else if(c == 'X') {
+                    sourceNode = n;
+                } else if (c == 'X') {
                     n.setTargetNode(true);    //target
                     ++targetNodeCount;              //test for multiple target nodes
+                    targetNode = n;
                 }
 
-                if(c != '#')                            //not blocked
+                if (c != '#')                            //not blocked
                     matrix.get(row).add(col, n);
                 else matrix.get(row).add(col, null);           //if blocked, it means no node is there
 
             }
         }
 
-        if(beginNodeCount > 1 || targetNodeCount > 1)           //more than one source/target node
+        if (beginNodeCount > 1 || targetNodeCount > 1)           //more than one source/target node
             return null;                        //return null for error
 
         //now we iterate through the matrix, connecting all the not-null nodes
 
 
-        for(int row = 0; row < matrix.size() - 1; ++row) {
+        for (int row = 0; row < matrix.size() - 1; ++row) {
             var nodes = matrix.get(row);
 
-            for(int col = 0; col < nodes.size() - 1; ++col) {
+            for (int col = 0; col < nodes.size() - 1; ++col) {
                 var currNode = nodes.get(col);
                 var rightNode = nodes.get(col + 1);
                 var belowNode = matrix.get(row + 1).get(col);
 
-                if(currNode != null) {
+                if (currNode != null) {
                     if (rightNode != null)
                         currNode.connect(rightNode);
-                    if(belowNode != null)
+                    if (belowNode != null)
                         currNode.connect(belowNode);
                 }
             }
@@ -76,13 +103,22 @@ public class MazeBuilder {
         //so now we can store them in a simple list
         //the connections remain the same
         //we add them to the maze
-        for(var list : matrix)
-            maze.getNodesList().addAll(list);
+        //the null Nodes are not included
+        for (var list : matrix) {
+            for(var node : list) {
+                if(node != null)
+                    maze.getNodesList().add(node);
+            }
+        }
 
-        //now we must set the begin and end nodes in the maze
+        //set the source and target nodes
+        maze.setBeginNode(sourceNode);
+        maze.setTargetNode(targetNode);
 
-        //for loop optimized not to iterate through all the nodes
+        //everything is done, return the maze
 
+        maze.setNodeMatrix(matrix);
 
+        return maze;
     }
 }
